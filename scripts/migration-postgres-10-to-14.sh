cf734f401236441cb92208b582cfc9da5076b355
Walter Seo <walter@protopie.io>
2025-05-12 11:51:49 +0900

    fix minors
    Signed-off-by: Walter Seo <walter@protopie.io>

--- scripts/migration-postgres-10-to-14.sh
+++ scripts/migration-postgres-10-to-14.sh
@@ -266,10 +266,10 @@ exit $?
 
 # --- Example Usage ---
 # Make sure you are logged into the correct AWS account and have kubectl access.
-# ./scripts/migrations/postgres-10-to-14-bitnami.sh <kube_context_arn> <namespace_name>
+# ./scripts/postgres-10-to-14-bitnami.sh <kube_context_arn> <namespace_name>
 #
 # Example for a specific namespace 'my-namespace' in Seoul (apne2):
-# ./scripts/migrations/postgres-10-to-14-bitnami.sh "arn:aws:eks:ap-northeast-2:794161565898:cluster/prod-seoul-cluster" "my-namespace"
+# ./scripts/postgres-10-to-14-bitnami.sh "arn:aws:eks:ap-northeast-2:794161565898:cluster/prod-seoul-cluster" "my-namespace"
 #
 # Example for a specific namespace 'another-ns' in Frankfurt (euc1):
-# ./scripts/migrations/postgres-10-to-14-bitnami.sh "arn:aws:eks:eu-central-1:794161565898:cluster/prod-frankfurt-cluster" "another-ns"
+# ./scripts/postgres-10-to-14-bitnami.sh "arn:aws:eks:eu-central-1:794161565898:cluster/prod-frankfurt-cluster" "another-ns"
